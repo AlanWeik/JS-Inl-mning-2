cf6d8779b370a0f8d26428a82c904e487b6eb568
Alan Weik <alaan.m@hotmail.com>
2021-02-11 00:48:59 +0100

    Minor html changes. Started building new API call in JS.

--- stuff.js
+++ stuff.js
@@ -1,11 +1,11 @@
 var button = document.querySelector('.button');
-//const input = document.querySelector('.searchbox');
+const input = document.querySelector('.searchbox');
 var main = document.querySelector('#name');
 var name = document.querySelector('.name');
 var desc = document.querySelector('.desc');
 var temp = document.querySelector('.temp');
 
-const input = "Marbella";
+//const input = "Marbella";
 const apiKey = "130c1f0ea968fc82e4f1a6eb80ca25e3";
 const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&units=metric`;
 
@@ -29,6 +29,38 @@ desc.innerHTML = "Desc: "+descValue;
 .catch(err => alert("An error occurred."))
 })
 
+
+//-----------------------NEW JS API CALL---------------------------------------------
+
+
+var button = document.querySelector('.button');
+//const input = document.querySelector('.searchbox');
+var main = document.querySelector('#name');
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
+const clientId = "EN5DJDGHU5COJS5BSROVHN0DYKHCGRGJ0FSWAJZC3TS0GT5T";
+const clientSecret = "PCK5DQLXAJ1H4LZKRF5T3M2ERT2LK3IXW2FRYTXMYPDZ53ZY";
+const fourUrl = `https://api.foursquare.com/v2/venues/explore&client_id=${clientId}&client_secret=${clientSecret}`;
+
+button.addEventListener('click',function(){
+    fetch(fourUrl)
+    .then(response => response.json())
+    .then(data => {
+    
+    var nameValue = data['name'];
+    var tempValue = data['main']['temp'];
+    var descValue = data['weather'][0]['description'];
+
+main.innerHTML = nameValue;
+temp.innerHTML = "Temp: "+tempValue+" C";
+desc.innerHTML = "Desc: "+descValue;
+
+})
+
+.catch(err => alert("An error occurred."))
+})
 
 
 
