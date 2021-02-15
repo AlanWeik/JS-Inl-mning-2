cb266665e879815d1ffbff9eae5033707f941d9d
Alan Weik <alaan.m@hotmail.com>
2021-02-15 19:09:16 +0100

    Removed longitude/latitue.

--- JS/printers.js
+++ JS/printers.js
@@ -15,7 +15,5 @@ const createWeatherHTML = (currentDay) =>
   return `<h2>${weekDays[(new Date()).getDay()]}</h2>
           <h2>Temperature: ${currentDay.main.temp}&deg;C</h2>
           <h2>Condition: ${currentDay.weather[0].description}</h2>
-      <h2>longitude: ${currentDay.coord.lon}</h2>
-      <h2>latitude: ${currentDay.coord.lat}</h2>
       <h2>Windspeed: ${currentDay.wind.speed} m/s</h2>`;
 };
