1542d909d392d044d17b3ffa6016b22a11e3dc06
Alan Weik <alaan.m@hotmail.com>
2021-02-15 17:06:59 +0100

    Minor syntax changes in "printers".

--- printers.js
+++ printers.js
@@ -7,9 +7,9 @@ const createVenueHTML = (name, location) => {
 };
 
 const createWeatherHTML = (currentDay) => {
-  console.log(currentDay);
-  return `<h2>${weekDays[new Date().getDay()]}</h2>
-          <h2>Temperature: ${currentDay.main.temp}&deg;F</h2>
+  console.log(currentDay)
+  return `<h2>${weekDays[(new Date()).getDay()]}</h2>
+          <h2>Temperature: ${currentDay.main.temp}&deg;C</h2>
           <h2>Condition: ${currentDay.weather[0].description}</h2>
       <h2>longitude: ${currentDay.coord.lon}</h2>
       <h2>latitude: ${currentDay.coord.lat}</h2>
