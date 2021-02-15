108facb8d6f7c7f39c30a2ea7083c905d68e5923
Alan Weik <alaan.m@hotmail.com>
2021-02-15 19:33:01 +0100

    Removed unnescessary CSS.

--- JS/printers.js
+++ JS/printers.js
@@ -13,7 +13,7 @@ const createWeatherHTML = (currentDay) =>
 {
   console.log(currentDay)
   return `<h2>${weekDays[(new Date()).getDay()]}</h2>
-          <h2>Temperature: ${currentDay.main.temp}&deg;C</h2>
+          <h2>Temperature: ${currentDay.main.temp}&deg;F</h2>
           <h2>Condition: ${currentDay.weather[0].description}</h2>
       <h2>Windspeed: ${currentDay.wind.speed} m/s</h2>`;
 };
